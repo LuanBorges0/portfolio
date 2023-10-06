eb90d23b1e6dfd55d05a94164c04ad0ee933b447
Luan Borges <luan.programador2023@gmail.com>
2023-10-06 14:29:24 -0300

    first commit

--- src/js/script.js
+++ src/js/script.js
@@ -20,83 +20,88 @@ function escrevendoLetra() {
 
 escrevendoLetra()
 
-
-function menuMobol(){
+function ativacaoMenu(){
     const ativaMenu = document.querySelector('.fa-bars');
-    const navMenu = document.querySelector('header .navegacao-primaria');
+    const navMenu = document.querySelector('header .navegacao-primaria')
 
-    ativaMenu.addEventListener('click', () =>{
-        ativaMenu.classList.toggle('fa-x');
-        navMenu.classList.toggle('ativado');
+
+    ativaMenu.addEventListener('click', ()=>{
+        ativaMenu.classList.toggle('fa-x')
+        navMenu.classList.toggle('ativado')
     })
-
 }
 
-menuMobol()
+ativacaoMenu()
 
 function sobreMim(){
-    const divExperiencia = document.querySelectorAll('.experience_content div');
-    const liExperiencia = document.querySelectorAll('.experience_content ul li');
-    const divEducation = document.querySelectorAll('.education_content div');
-    const liEducation = document.querySelectorAll('.education_content ul li');
+    const experiencia = document.querySelectorAll('.experience_content div');
+    const botao = document.querySelectorAll('.experience_content ul li')
+    const education = document.querySelectorAll('.education_content div');
+    const botaoEducation = document.querySelectorAll('.education_content ul li')
 
-    divExperiencia[0].classList.add('ativo')
-    divEducation[0].classList.add('ativo')
-    liExperiencia[0].classList.add('ativo')
-    liEducation[0].classList.add('ativo')
 
+    experiencia[0].classList.add('ativo')
+    botao[0].classList.add('ativo')
+    education[0].classList.add('ativo')
+    botaoEducation[0].classList.add('ativo')
 
     function slideShow(index){
-        divExperiencia.forEach((div)=>{
-            div.classList.remove('ativo');
+        experiencia.forEach((divisao)=>{
+            divisao.classList.remove('ativo');
         });
-        liExperiencia.forEach((botao)=>{
-            botao.classList.remove('ativo');
-        })
-        divExperiencia[index].classList.add('ativo');
-        liExperiencia[index].classList.add('ativo');
+        botao.forEach((item)=>{
+            item.classList.remove('ativo')
+        });
+        experiencia[index].classList.add('ativo')
+        botao[index].classList.add('ativo')
     }
 
     function slideShow2(index){
-        divEducation.forEach((div)=> {
-            div.classList.remove('ativo');
+        education.forEach((divisao)=>{
+            divisao.classList.remove('ativo');
         });
-        liEducation.forEach((botao)=> {
-            botao.classList.remove('ativo');
-        })
-        divEducation[index].classList.add('ativo');
-        liEducation[index].classList.add('ativo');
+        botaoEducation.forEach((item)=>{
+            item.classList.remove('ativo')
+        });
+        education[index].classList.add('ativo')
+        botaoEducation[index].classList.add('ativo')
     }
 
 
-    liExperiencia.forEach((event,index)=>{
-        event.addEventListener('click', () => {
-            slideShow(index);
+    botao.forEach((event,index)=>{
+        event.addEventListener('click', ()=>{
+            slideShow(index)
         });
     });
 
-    liEducation.forEach((event,index)=>{
-        event.addEventListener('click', () => {
-            slideShow2(index);
-        });
-    });
+    botaoEducation.forEach((div, index)=>{
+        div.addEventListener('click', ()=>{
+            slideShow2(index)
+        })
+    })
 }
-
 sobreMim();
 
-const listaALL = document.querySelectorAll('.project_armazenamento ul li');
-const buttonGeral = document.querySelectorAll('.projects_models ul li');
-const buttonALL = document.querySelectorAll('.projects_models .all');
+
+
+
+const listaALL = document.querySelectorAll('.projects_armazenamento ul li');
+const buttonGeral = document.querySelectorAll('.project_navegacao li');
+const buttonALL = document.querySelector('.project_models .all');
+
+listaALL.forEach((item)=>{
+    item.classList.add('ativo');
+})
 
 function removeClick(index){
     buttonGeral.forEach((item)=>{
         item.classList.remove('ativo');
-    });
-    buttonGeral[index].classList.add('ativo');
+    })
+    buttonGeral[index].classList.add('ativo')
 }
 
-buttonGeral.forEach((item,index)=>{
-    item.addEventListener('click', ()=>{
+buttonGeral.forEach((event,index)=>{
+    event.addEventListener('click', ()=>{
         removeClick(index)
     })
 })
@@ -104,10 +109,33 @@ buttonGeral.forEach((item,index)=>{
 function showLista(lista, buttom = "all"){
     lista.forEach((item)=>{
         item.classList.remove('ativo');
-    })
+    });
+
     if(buttom == 'design'){
-        lista[0].classList.add('ativo');
-        lista[1].classList.add('ativo');
+        lista[0].classList.add('ativo')
+        lista[1].classList.add('ativo')
+    }
+    if(buttom == 'graphic'){
+        lista[2].classList.add('ativo');
+        lista[3].classList.add('ativo');
+    }
+
+    if(buttom == 'website'){
+        lista[4].classList.add('ativo');
+        lista[5].classList.add('ativo');
+        lista[6].classList.add('ativo');
+        lista[7].classList.add('ativo');
+    }
+
+    if(buttom == 'all'){
+        lista[0].classList.add('ativo')
+        lista[1].classList.add('ativo')
+        lista[2].classList.add('ativo');
+        lista[3].classList.add('ativo');
+        lista[4].classList.add('ativo');
+        lista[5].classList.add('ativo');
+        lista[6].classList.add('ativo');
+        lista[7].classList.add('ativo');
     }
 }
 
@@ -116,18 +144,20 @@ buttonGeral.forEach((item)=>{
         let currentButton = e.target;
         if(currentButton.classList.contains('all')){
             showLista(listaALL);
+        } if(currentButton.classList.contains('design')){
+            showLista(listaALL, "design")
         }
-        if(currentButton.classList.contains('design')){
-            showLista(listaALL, "design");
-        }
+
         if(currentButton.classList.contains('graphic')){
-            showLista(listaALL, "graphic");
+            showLista(listaALL, "graphic")
         }
+
         if(currentButton.classList.contains('website')){
-            showLista(listaALL, "website");
+            showLista(listaALL, "website")
         }
+
         if(currentButton.classList.contains('all')){
-            showLista(listaALL, "all");
+            showLista(listaALL, "all")
         }
-    })
-})
+    });
+});
